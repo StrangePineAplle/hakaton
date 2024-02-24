9a18ebea6509ab4d041a6fd69545c6e7c56ac85f
Alexander <alexandr.borisov31@gmail.com>
2024-02-24 13:54:44 +0300

    pretty message

--- bot/handlers.py
+++ bot/handlers.py
@@ -8,4 +8,9 @@ router = Router()
 
 @router.message(Command("start"))
 async def start(message: types.Message) -> None:
-    await message.answer("Переход на сайт", reply_markup=keyboard)
+    await message.answer(
+        "<b>Давайте начнём!</b>\n\n"
+        "Пожалуйста, нажмите на кнопку ниже, чтобы перейти в приложение",
+        reply_markup=keyboard,
+        parse_mode="HTML",
+    )
